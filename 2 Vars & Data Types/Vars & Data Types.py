422043382a54651fa1fc80e2543056a81ae288c8
Emanuel Joness <itzemanuelj@gmail.com>
2023-04-14 11:02:49 -0400

    mvp commit for vars and data types section 2

---
+++ 2 Vars & Data Types/Vars & Data Types.py
@@ -0,0 +1,40 @@
+## Data Types
+
+## string concatation
+
+name = "Justin"
+age = 28
+
+print("my name is " + str(name)+" my age is " + str(age)+ " years old")
+print(f"my name is {name} i am {age} years old")
+##print(name + age)
+
+## int
+age = 28
+year = 1995
+cost = 1
+
+print(f"i am {age} years old")
+print(f"i was born in {year}")
+print(f"the price is {cost} dollar")
+
+## float
+gallon = 10.5
+miles = 5
+weight = 250
+
+print(f"i have about {gallon} gallons left")
+print(f"the game is {miles} miles away")
+print(f"your current weight: {weight}")
+
+
+## string
+print("hello world")
+
+
+##bool
+is_online = True
+is_logged_in = False
+
+print(f"user100 is online:{is_online}")
+print(f"user102 is logged in:{is_logged_in}")
